b89930ee621f3611908eb63f76edf4c1d430be1a
moskalyk <morgan.moskalyk@protonmal.ch>
2021-12-14 21:00:29 -0500

    verkle chain?

--- server.js
+++ server.js
@@ -5,47 +5,51 @@ const Hyperbee = require('hyperbee')
 const SDK = require('hyper-sdk')
 const {DB} = require('hyperbeedeebee')
 const bodyParser = require('body-parser')
-let db;
 
-async function bootstrap(){
-	const {Hypercore} = await SDK()
+const DB_NAME = 'AKASHIC'
+let init = require('./init.js')
 
-	// Initialize a hypercore for loading data
-	const core = new Hypercore('example')
-	// Initialize the Hyperbee you want to use for storing data and indexes
-	const bee = new Hyperbee(core)
+const bootstrap = async () => {
 
-	// Create a new DB
-	db = new DB(bee)
-}
+	let records = await init(DB_NAME)
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
-bootstrap()
+	app.use(bodyParser.json())
+	app.use(bodyParser.urlencoded({extended:false}))
 
-app.post('/pull', async (req,res) => {
-	console.log(req.body)
-	const cid = req.body.cid
+	console.log(records)
 
-	const doc = await db.collection('example').insert({
-	  cid: cid
+	app.post('/pull', async (req,res) => {
+		console.log(req.body)
+		const cid = req.body.cid
+
+		const doc = await records.db.collection(DB_NAME).insert({
+		  cid: cid
+		})
+
+		res.send(doc)
 	})
 
-	const docs = await db.collection('example').find({cid: cid})
-	res.send(docs)
-})
+	app.get('/akashic/:id', async (req,res) => {
+		const query = req.params.id
+		console.log(query)
+		const docs = await records.db.collection(DB_NAME).find({cid: query})
+		res.send(docs)
+	})
 
-app.get('/akashic/:id', async (req,res) => {
-	const query = req.params.id
-	console.log(query)
-	const docs = await db.collection('example').find({cid: query})
-	res.send(docs)
-})
+	app.get('/akashic', async (req,res) => {
+		const docs = await records.db.collection(DB_NAME).find({
+		  cid: {
+		    $exists: true
+		  }
+		})
+		res.send(docs)
+	})
 
-app.get('/akashic/full', (req,res) => {
-	res.send([])
-})
+	app.listen(1440, () => {
+		console.log('listening')
+		const url = `hyper://${records.core.key.toString('hex')}`
+  		console.log(url)
+	})
+}
 
-app.listen(1440, () => {
-	console.log('listening')
-})
+bootstrap()
